20604f1e16e15cf99e27653305586145ee0f590e
Dgraber27 <drewg2727@gmail.com>
2020-09-21 17:34:12 -0400

    Edit

--- RestaurantRaterSecond/Controllers/RestaurantController.cs
+++ RestaurantRaterSecond/Controllers/RestaurantController.cs
@@ -1,6 +1,8 @@
-﻿using RestaurantRaterSecond.Models;
+﻿using Microsoft.Ajax.Utilities;
+using RestaurantRaterSecond.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Web;
@@ -64,6 +66,40 @@ namespace RestaurantRaterSecond.Controllers
             _db.Restaurants.Remove(restaurant);
             _db.SaveChanges();
             return RedirectToAction("Index");
+
+        }
+
+        //GET: Restaurant/Edit/{id}
+        //Get Ud frin yser
+        //Find restaurant by Id
+        //If restaurant doesn't exits
+        //Return restaurant and view
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Restaurant restaurant = _db.Restaurants.Find(id);
+            if (restaurant == null)
+            {
+                return HttpNotFound();
+            }
+            return View(restaurant);
+        }
+
+        //Post: Restaurant/Edit/{id}
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit (Restaurant restaurant)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Entry(restaurant).State = EntityState.Modified;
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(restaurant);
 
         }
     }
